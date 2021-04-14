3415b6919d457619daca342572afa8f83b1ef675
Gianluca Arbezzano <gianarb92@gmail.com>
2021-04-14 11:08:13 +0000

    Check actions who needs to be re-build
    As part of the continuous integration workflows I would like to know
    which actions will get build as soon as the pull request will be merged.
    
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

--- hack/ci-check.sh
+++ hack/ci-check.sh
@@ -4,8 +4,6 @@
 
 set -eux
 
-env
-
 failed=0
 
 if ! git ls-files '*.sh' | xargs shfmt -l -d; then
@@ -26,3 +24,12 @@ test -z "$(git status --porcelain go.mod go.sum)"
 go vet ./...
 
 go test -v ./...
+
+# We check the list of actions to rebuild only for the entire pull_request.
+# The push event does not have a GITHUB_BASE_REF set. That's why here we use
+# that environment variable to figure out if it is a PR event or a commit push
+if [[ -z $GITHUB_BASE_REF ]]; then
+	echo "Skipping: This should only run on pull_request."
+	exit 0
+fi
+go run cmd/hub/main.go build --dry-run --git-ref "origin/$GITHUB_HEAD_REF..origin/$GITHUB_BASE_REF"
