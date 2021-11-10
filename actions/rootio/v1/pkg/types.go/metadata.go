63241a928857a701bbbcefee4171a353dbf0fbb8
gmx <gmxdrone@gmail.com>
2021-11-10 09:41:25 -0500

    enable processing of hardware.json
    Signed-off-by: alienninja <39408173+alienninja@users.noreply.github.com>

--- actions/rootio/v1/pkg/types.go/metadata.go
+++ actions/rootio/v1/pkg/types.go/metadata.go
@@ -9,9 +9,91 @@ import (
 	"time"
 )
 
+type ExportedCacher struct {
+	ID                                 string                   `json:"id"`
+	Metadata                           Metadata                 `jsonm:"metadata"`
+}
+
+type Metadata struct {
+	Arch                               string                   `json:"arch"`
+	State                              string                   `json:"state"`
+	EFIBoot                            bool                     `json:"efi_boot"`
+	Instance                           Instance                 `json:"instance,omitempty"`
+	PreinstalledOperatingSystemVersion interface{}              `json:"preinstalled_operating_system_version"`
+	NetworkPorts                       []map[string]interface{} `json:"network_ports"`
+	PlanSlug                           string                   `json:"plan_slug"`
+	Facility                           string                   `json:"facility_code"`
+	Hostname                           string                   `json:"hostname"`
+	BondingMode                        int                      `json:"bonding_mode"`
+}
+
+type Instance struct {
+	ID       string `json:"id,omitempty"`
+	State    string `json:"state,omitempty"`
+	Hostname string `json:"hostname,omitempty"`
+	AllowPXE bool   `json:"allow_pxe,omitempty"`
+	Rescue   bool   `json:"rescue,omitempty"`
+
+	IPAddresses []map[string]interface{} `json:"ip_addresses,omitempty"`
+	OS          OperatingSystem         `json:"operating_system_version,omitempty"`
+	UserData    string                   `json:"userdata,omitempty"`
+
+	CryptedRootPassword string `json:"crypted_root_password,omitempty"`
+
+	Storage      Storage `json:"storage,omitempty"`
+	SSHKeys      []string `json:"ssh_keys,omitempty"`
+	NetworkReady bool     `json:"network_ready,omitempty"`
+}
+
+type OperatingSystem struct {
+	Slug     string `json:"slug"`
+	Distro   string `json:"distro"`
+	Version  string `json:"version"`
+	ImageTag string `json:"image_tag"`
+	OsSlug   string `json:"os_slug"`
+}
+
+
+
+type File struct {
+	Path     string `json:"path"`
+	Contents string `json:"contents,omitempty"`
+	Mode     int    `json:"mode,omitempty"`
+	UID      int    `json:"uid,omitempty"`
+	GID      int    `json:"gid,omitempty"`
+}
+
+//type Filesystem struct {
+//	Mount struct {
+//		Device string             `json:"device"`
+//		Format string             `json:"format"`
+//		Files  []*File            `json:"files,omitempty"`
+//		Create *FilesystemOptions `json:"create,omitempty"`
+//		Point  string             `json:"point"`
+//	} `json:"mount"`
+//}
+
+type FilesystemOptions struct {
+	Force   bool     `json:"force,omitempty"`
+	Options []string `json:"options,omitempty"`
+}
+
+type Raid struct {
+	Name    string   `json:"name"`
+	Level   string   `json:"level"`
+	Devices []string `json:"devices"`
+	Spares  int      `json:"spares,omitempty"`
+}
+
+type Storage struct {
+	Disks       []Disk       `json:"disks,omitempty"`
+	RAID        []Raid       `json:"raid,omitempty"`
+	Filesystems []Filesystem `json:"filesystems,omitempty"`
+}
+
 // Metadata struct
 // This is an auto generated struct taken from a metadata request
-type Metadata struct {
+type Metadata_Instance struct {
 	CryptedRootPassword    string `json:"crypted_root_password"`
 	Hostname               string `json:"hostname"`
 	OperatingSystemVersion struct {
@@ -28,7 +110,7 @@ type Metadata struct {
 
 //Filesystem defines the organisation of a filesystem
 type Filesystem struct {
-	Mount struct {
+		Mount struct {
 		Create struct {
 			Options []string `json:"options"`
 		} `json:"create"`
@@ -53,7 +135,7 @@ type Partitions struct {
 }
 
 //RetreieveData -
-func RetreieveData() (*Metadata, error) {
+func RetreieveData() (*Instance, error) {
 	metadataURL := os.Getenv("MIRROR_HOST")
 	if metadataURL == "" {
 		return nil, fmt.Errorf("Unable to discover the metadata server from environment variable [MIRROR_HOST]")
@@ -84,12 +166,13 @@ func RetreieveData() (*Metadata, error) {
 		return nil, err
 	}
 
-	var mdata Metadata
+	var exportedcacher ExportedCacher
+	//var mdata Metadata
 
-	jsonErr := json.Unmarshal(body, &mdata)
+	jsonErr := json.Unmarshal(body, &exportedcacher)
 	if jsonErr != nil {
 		return nil, jsonErr
 	}
 
-	return &mdata, nil
+	return &exportedcacher.Metadata.Instance, nil
 }
