813513162481ba4982917bcbec857df8a32c9805
Jacob Weinstock <jakobweinstock@gmail.com>
2024-02-20 00:27:57 -0700

    Remove tools module:
    Use Make goal to handle instead.
    Its less code and simpler.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@ all: $(BINS)
 $(BINS):
 	make -C $@
 
-formatters: bin/gofumpt bin/goimports
+include rules.mk
+include lint.mk
+
+formatters: $(toolBins)
 	git ls-files '*.go' | xargs -I% sh -c 'sed -i "/^import (/,/^)/ { /^\s*$$/ d }" % && bin/gofumpt -w %'
 	git ls-files '*.go' | xargs -I% bin/goimports -w %
 
 tidy-all:
 	(cd tools; go mod tidy)
 	for d in $(BINS); do (cd $$d; go mod tidy); done
-
-include lint.mk
-include rules.mk
--- rules.mk
+++ rules.mk
@@ -5,10 +5,10 @@ PATH := ${GOBIN}:${PATH}
 export PATH
 endif
 
-toolsBins := $(addprefix bin/,$(notdir $(shell awk -F'"' '/^\s*_/ {print $$2}' tools/tools.go)))
 
-# installs cli tools defined in tools/tools.go
-$(toolsBins): tools/go.mod tools/go.sum tools/tools.go
-$(toolsBins): CMD=$(shell awk -F'"' '/$(@F)"/ {print $$2}' tools/tools.go)
-$(toolsBins):
-	go install $(CMD)
+toolLibs = golang.org/x/tools/cmd/goimports mvdan.cc/gofumpt
+toolBins = $(addprefix bin/,$(notdir $(toolLibs)))
+
+# installs cli tools
+$(toolBins):
+	go install $(filter %$(notdir $@),$(toolLibs))@latest
