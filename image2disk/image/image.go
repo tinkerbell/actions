5c1aea55bfa5c68c6ccdb76f4286dce98d6497d1
EC2 Default User <ec2-user@ip-172-31-5-39.ca-central-1.compute.internal>
2024-08-05 21:21:47 +0000

    retry logic for image download

--- image2disk/image/image.go
+++ image2disk/image/image.go
@@ -47,81 +47,105 @@ func tickerProgress(byteCounter uint64) {
 // with compress set to true it will use gzip compression to expand the data before
 // writing to an underlying device.
 func Write(sourceImage, destinationDevice string, compressed bool) error {
+	const maxRetries = 5
+	const retryDelay = 10 * time.Second
+
+	var err error
+	var resp *http.Response
+	var fileOut *os.File
+	var out io.Reader
+
+	for attempt := 0; attempt < maxRetries; attempt++ {
+		resp, err = fetchImage(sourceImage)
+		if err != nil {
+			if attempt < maxRetries-1 {
+				log.Warnf("Failed to fetch image [%s]: %v. Retrying in %v...", sourceImage, err, retryDelay)
+				time.Sleep(retryDelay)
+				continue
+			}
+			return fmt.Errorf("failed to fetch image [%s] after %d retries: %w", sourceImage, maxRetries, err)
+		}
+		defer resp.Body.Close() // Ensure body is closed when done
+
+		fileOut, err = os.OpenFile(destinationDevice, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+		if err != nil {
+			return err
+		}
+		defer fileOut.Close()
+
+		if !compressed {
+			// Without compression send raw output
+			out = resp.Body
+		} else {
+			// Find compression algorithm based upon extension
+			decompressor, err := findDecompressor(sourceImage, resp.Body)
+			if err != nil {
+				return err
+			}
+			defer decompressor.Close()
+			out = decompressor
+		}
+
+		log.Infof("Beginning write of image [%s] to disk [%s]", filepath.Base(sourceImage), destinationDevice)
+
+		ticker := time.NewTicker(500 * time.Millisecond)
+		counter := &WriteCounter{}
+		go func() {
+			for range ticker.C {
+				tickerProgress(counter.Total)
+			}
+		}()
+
+		if _, err = io.Copy(fileOut, io.TeeReader(out, counter)); err != nil {
+			ticker.Stop()
+			if attempt < maxRetries-1 {
+				log.Warnf("Failed to write data, retrying... [%v]", err)
+				time.Sleep(retryDelay)
+				continue
+			}
+			return fmt.Errorf("failed to write image after %d retries: %w", maxRetries, err)
+		}
+
+		ticker.Stop()
+		fmt.Printf("\n")
+
+		// Do the equivalent of partprobe on the device
+		if err := fileOut.Sync(); err != nil {
+			return fmt.Errorf("failed to sync the block device: %w", err)
+		}
+
+		if err := unix.IoctlSetInt(int(fileOut.Fd()), unix.BLKRRPART, 0); err != nil {
+			// Ignore errors since it may be a partition, but log in case it's helpful
+			log.Errorf("Error re-probing the partitions for the specified device: %v", err)
+		}
+
+		return nil
+	}
+
+	return fmt.Errorf("failed to write image after %d retries", maxRetries)
+}
+
+// fetchImage fetches the image from the specified URL.
+func fetchImage(sourceImage string) (*http.Response, error) {
 	req, err := http.NewRequestWithContext(context.TODO(), "GET", sourceImage, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	defer resp.Body.Close()
 
 	if resp.StatusCode > 300 {
-		// Customise response for the 404 to make degugging simpler
+		// Customize response for the 404 to make debugging simpler
 		if resp.StatusCode == 404 {
-			return fmt.Errorf("%s not found", sourceImage)
+			return nil, fmt.Errorf("%s not found", sourceImage)
 		}
-		return fmt.Errorf("%s", resp.Status)
+		return nil, fmt.Errorf("%s", resp.Status)
 	}
 
-	var out io.Reader
-
-	fileOut, err := os.OpenFile(destinationDevice, os.O_WRONLY, 0o644)
-	if err != nil {
-		return err
-	}
-	defer fileOut.Close()
-
-	if !compressed {
-		// Without compression send raw output
-		out = resp.Body
-	} else {
-		// Find compression algorithm based upon extension
-		decompressor, err := findDecompressor(sourceImage, resp.Body)
-		if err != nil {
-			return err
-		}
-		defer decompressor.Close()
-		out = decompressor
-	}
-
-	log.Infof("Beginning write of image [%s] to disk [%s]", filepath.Base(sourceImage), destinationDevice)
-	// Create our progress reporter and pass it to be used alongside our writer
-	ticker := time.NewTicker(500 * time.Millisecond)
-	counter := &WriteCounter{}
-
-	go func() {
-		for ; true; <-ticker.C {
-			tickerProgress(counter.Total)
-		}
-	}()
-	if _, err = io.Copy(fileOut, io.TeeReader(out, counter)); err != nil {
-		ticker.Stop()
-		return err
-	}
-
-	count, err := io.Copy(fileOut, out)
-	if err != nil {
-		ticker.Stop()
-		return fmt.Errorf("error writing %d bytes to disk [%s] -> %w", count, destinationDevice, err)
-	}
-	fmt.Printf("\n")
-
-	ticker.Stop()
-
-	// Do the equivalent of partprobe on the device
-	if err := fileOut.Sync(); err != nil {
-		return fmt.Errorf("failed to sync the block device")
-	}
-
-	if err := unix.IoctlSetInt(int(fileOut.Fd()), unix.BLKRRPART, 0); err != nil {
-		// Ignore errors since it may be a partition, but log in case it's helpful
-		log.Errorf("error re-probing the partitions for the specified device: %v", err)
-	}
-
-	return nil
+	return resp, nil
 }
 
 func findDecompressor(imageURL string, r io.Reader) (io.ReadCloser, error) {
