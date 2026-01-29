92ab3f983912203eb27c7242d41fbd0494fb9884
Sebastian Thees <thees@users.noreply.github.com>
2026-01-29 16:07:49 +0100

    oci2disk: add the ability to pass the compression algorithm via the COMPRESSED environment variable so that the image tag does not need to have the compression extension as a "file extension". still keep the backward compatibility with COMPRESSED=true so that the imageUrl (i.e. the image tag) is parsed for the compression algorithm
    Signed-off-by: Sebastian Thees <thees@users.noreply.github.com>

--- oci2disk/image/image.go
+++ oci2disk/image/image.go
@@ -35,10 +35,59 @@ func (wc *WriteCounter) Write(p []byte) (int, error) {
 	return n, nil
 }
 
-// Write will pull an image and write it to local storage device
-// with compress set to true it will use gzip compression to expand the data before
-// writing to an underlying device.
-func Write(sourceImage, destinationDevice string, compressed bool) error {
+type Compression int
+
+const (
+	CompressionNone Compression = iota
+	CompressionGzip
+	CompressionXZ
+	CompressionZstd
+	CompressionBzip2
+)
+
+// ParseCompression parses a compression string into a Compression type.
+// Accepts: gz, gzip, xz, zstd, zst, bzip2
+// For backward compatibility: "true" detects compression from the imageURL extension.
+// Empty string or "false" returns CompressionNone.
+func ParseCompression(s string, imageURL string) Compression {
+	switch s {
+	case "gz", "gzip":
+		return CompressionGzip
+	case "xz":
+		return CompressionXZ
+	case "zstd", "zst", "zs":
+		return CompressionZstd
+	case "bzip2":
+		return CompressionBzip2
+	case "true":
+		// Backward compatibility: detect from image URL extension
+		return detectCompressionFromURL(imageURL)
+	default:
+		return CompressionNone
+	}
+}
+
+// detectCompressionFromURL detects compression type from URL file extension.
+// This provides backward compatibility with COMPRESSED=true.
+func detectCompressionFromURL(imageURL string) Compression {
+	switch filepath.Ext(imageURL) {
+	case ".bzip2":
+		return CompressionBzip2
+	case ".gz":
+		return CompressionGzip
+	case ".xz":
+		return CompressionXZ
+	case ".zstd", ".zst", ".zs":
+		return CompressionZstd
+	default:
+		return CompressionNone
+	}
+}
+
+// Write will pull an image and write it to local storage device.
+// If compression is not CompressionNone, it will decompress the data before
+// writing to the underlying device.
+func Write(sourceImage, destinationDevice string, compression Compression) error {
 	ctx := context.Background()
 	client := http.DefaultClient
 	opts := docker.ResolverOptions{}
@@ -60,7 +109,7 @@ func Write(sourceImage, destinationDevice string, compressed bool) error {
 	customMediaType := "application/vnd.oci.image.layer.v1.tar"
 	allowedMediaTypes := []string{customMediaType}
 
-	f := NewDiskImageStore(sourceImage, compressed, fileOut)
+	f := NewDiskImageStore(compression, fileOut)
 
 	log.Infof("Beginning write of image [%s] to disk [%s]", filepath.Base(sourceImage), destinationDevice)
 	pullOpts := []oras.PullOpt{
@@ -87,29 +136,31 @@ func Write(sourceImage, destinationDevice string, compressed bool) error {
 	return nil
 }
 
-func findDecompressor(imageURL string, r io.Reader) (io.ReadCloser, error) {
-	switch filepath.Ext(imageURL) {
-	case ".bzip2":
+func newDecompressor(compression Compression, r io.Reader) (io.ReadCloser, error) {
+	switch compression {
+	case CompressionBzip2:
 		return io.NopCloser(bzip2.NewReader(r)), nil
-	case ".gz":
+	case CompressionGzip:
 		reader, err := gzip.NewReader(r)
 		if err != nil {
 			return nil, fmt.Errorf("[ERROR] New gzip reader: %w", err)
 		}
 		return reader, nil
-	case ".xz":
+	case CompressionXZ:
 		reader, err := xz.NewReader(r)
 		if err != nil {
 			return nil, fmt.Errorf("[ERROR] New xz reader: %w", err)
 		}
 		return io.NopCloser(reader), nil
-	case ".zs":
+	case CompressionZstd:
 		reader, err := zstd.NewReader(r)
 		if err != nil {
 			return nil, fmt.Errorf("[ERROR] New zs reader: %w", err)
 		}
 		return reader.IOReadCloser(), nil
+	case CompressionNone:
+		return io.NopCloser(r), nil
 	}
 
-	return nil, fmt.Errorf("unknown compression suffix [%s]", filepath.Ext(imageURL))
+	return nil, fmt.Errorf("[ERROR] Unknown compression type: %d", compression)
 }
